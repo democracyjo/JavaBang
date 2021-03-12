4281aafb49f71381f7417fcacfd509c40871ca27
제주집게벌레 <@DESKTOP-DA90KRE>
2021-03-12 19:56:38 +0900

    민주 wish/reser/pay -- view단까지

--- src/kosta/mvc/controller/PayControllerImpl.java
+++ src/kosta/mvc/controller/PayControllerImpl.java
@@ -1,27 +1,46 @@
 package kosta.mvc.controller;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import kosta.mvc.model.dto.Pay;
+import kosta.mvc.model.service.PayService;
+import kosta.mvc.model.service.PayServiceImpl;
+import kosta.mvc.view.FailView;
+import kosta.mvc.view.SuccessView;
 
 public class PayControllerImpl implements PayController {
+	private PayService payService = new PayServiceImpl();
 
 	@Override
-	public void insertPay(Pay pay) throws SQLException {
-		// TODO Auto-generated method stub
-
+	public void insertPay(Pay pay)  {
+		try {
+			payService.insertPay(pay);
+			SuccessView.messagePrint("결제리스트에 등록되었습니다.");
+		} catch (SQLException e) {
+			FailView.errorMessage(e.getMessage());
+		}
 	}
 
 	@Override
-	public void payDelete(int payNo) throws SQLException {
-		// TODO Auto-generated method stub
-
+	public void payDelete(int payNo)  {
+		try {
+			payService.payDelete(payNo);
+			SuccessView.messagePrint(payNo+"번 결제정보 삭제되었습니다.");
+		} catch (SQLException e) {
+			FailView.errorMessage(e.getMessage());
+		}
+		
 	}
 
 	@Override
-	public void selectPayByUserId(String userId) throws SQLException {
-		// TODO Auto-generated method stub
-
+	public void selectPayByUserId(String userId) {
+		try {
+			List<Pay> payList = payService.selectPayByUserId(userId);
+			SuccessView.selectPayByUserIdPrint(payList);
+		} catch (SQLException e) {
+			FailView.errorMessage(e.getMessage());
+		}
 	}
 
 }
--- src/kosta/mvc/controller/RsrvtControllerImpl.java
+++ src/kosta/mvc/controller/RsrvtControllerImpl.java
@@ -1,27 +1,46 @@
 package kosta.mvc.controller;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import kosta.mvc.model.dto.Reservation;
+import kosta.mvc.model.service.RsrvtService;
+import kosta.mvc.model.service.RsrvtServiceImpl;
+import kosta.mvc.view.FailView;
+import kosta.mvc.view.SuccessView;
 
 public class RsrvtControllerImpl implements RsrvtController {
+	private RsrvtService reserService = new RsrvtServiceImpl();
+	
 
 	@Override
-	public void insertReservation(Reservation rsrvt) throws SQLException {
-		// TODO Auto-generated method stub
-
+	public void insertReservation(Reservation rsrvt) {
+		try {
+			reserService.insertReservation(rsrvt);
+			SuccessView.messagePrint("예약되었습니다.");
+		} catch (SQLException e) {
+			FailView.errorMessage(e.getMessage());
+		}
 	}
 
 	@Override
-	public void reservationDelete(int reserNo) throws SQLException {
-		// TODO Auto-generated method stub
-
+	public void reservationDelete(int reserNo)  {
+		try {
+			reserService.reservationDelete(reserNo);
+			SuccessView.messagePrint(reserNo+"번 예약 삭제되었습니다.");
+		} catch (SQLException e) {
+			FailView.errorMessage(e.getMessage());
+		}
 	}
 
 	@Override
-	public void selectRsrvtByUserId(String userId) throws SQLException {
-		// TODO Auto-generated method stub
-
+	public void selectRsrvtByUserId(String userId) {
+		try {
+			List<Reservation> reserList = reserService.selectRsrvtByUserId(userId);
+			SuccessView.selectRsrvtByUserId(reserList);
+		} catch (SQLException e) {
+			FailView.errorMessage(e.getMessage());
+		}
 	}
 
 }
