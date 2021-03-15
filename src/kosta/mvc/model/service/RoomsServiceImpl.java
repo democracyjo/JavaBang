91ba1d245e9da696469537f1802c403906921ac5
ckdud <ckdud@DESKTOP-M8JRTA8>
2021-03-15 17:52:23 +0900

    sdg

--- src/kosta/mvc/model/service/RoomsServiceImpl.java
+++ src/kosta/mvc/model/service/RoomsServiceImpl.java
@@ -14,15 +14,20 @@ import kosta.mvc.model.dto.Room;
 
 public class RoomsServiceImpl implements RoomsService{
 	RoomsDAO roomDAO=new RoomsDAOImpl();
+	ReviewService rs=new ReviewServiceImpl();
 	
 	public List<Room> selectAll() throws SQLException{
-		
-		return roomDAO.selectAll();
+		List<Room> list= roomDAO.selectAll();
+		for(Room rm : list) {
+			rm.setAvgScore(rs.getAvgP(rm.getRoomNo()));
+		}
+		return list;
 	}
 	
 	public Room searchByRoomNo(int roomNo) throws Exception{
 		Room rm= roomDAO.searchByRoomNo(roomNo);
 		if(rm!=null) {
+			rm.setAvgScore(rs.getAvgP(rm.getRoomNo()));
 			return rm;
 		}else {
 			throw new NotFoundException("검색 한 방 번호가 없습니다");
@@ -31,6 +36,10 @@ public class RoomsServiceImpl implements RoomsService{
 	public List<Room> searchByRoomType(List<String> roomType) throws Exception{
 		List<Room> list=roomDAO.searchByRoomType(roomType);
 		if(list!=null) {
+			for(Room rm : list) {
+				rm.setAvgScore(rs.getAvgP(rm.getRoomNo()));
+			}
+
 			return list;
 		}else {
 			throw new NotFoundException("검색 한 방타입이 없습니다");
@@ -41,6 +50,10 @@ public class RoomsServiceImpl implements RoomsService{
 	public List<Room> searchByRoomSize(double minSize,double maxSize) throws Exception{
 		List<Room> list= roomDAO.searchByRoomSize(minSize, maxSize);
 		if(list!=null) {
+			for(Room rm : list) {
+				rm.setAvgScore(rs.getAvgP(rm.getRoomNo()));
+			}
+
 			return list;
 		}else {
 			throw new NotFoundException("검색 한 방이 없습니다");
@@ -49,6 +62,10 @@ public class RoomsServiceImpl implements RoomsService{
 	public List<Room> searchByRoomPrice(int minPrice,int maxPrice) throws Exception{
 		List<Room> list= roomDAO.searchByRoomPrice(minPrice, maxPrice);
 		if(list!=null) {
+			for(Room rm : list) {
+				rm.setAvgScore(rs.getAvgP(rm.getRoomNo()));
+			}
+
 			return list;
 		}else {
 			throw new NotFoundException("검색 한 방이 없습니다");
@@ -57,6 +74,10 @@ public class RoomsServiceImpl implements RoomsService{
 	public List<Room> searchByFloor(int floor) throws Exception{
 		List<Room> list= roomDAO.searchByFloor(floor);
 		if(list!=null) {
+			for(Room rm : list) {
+				rm.setAvgScore(rs.getAvgP(rm.getRoomNo()));
+			}
+
 			return list;
 		}else {
 			throw new NotFoundException("검색 한 방이 없습니다");
@@ -67,6 +88,10 @@ public class RoomsServiceImpl implements RoomsService{
 		List<Room> list= roomDAO.searchByNumberBeds(minNum, maxNum);
 		
 		if(list!=null) {
+			for(Room rm : list) {
+				rm.setAvgScore(rs.getAvgP(rm.getRoomNo()));
+			}
+
 			return list;
 		}else {
 			throw new NotFoundException("검색 한 방이 없습니다");
@@ -79,6 +104,10 @@ public class RoomsServiceImpl implements RoomsService{
 		// TODO Auto-generated method stub
 		List<Room> list= roomDAO.searchByNumberPeople(minNbPeople, maxNbPeople);
 		if(list!=null) {
+			for(Room rm : list) {
+				rm.setAvgScore(rs.getAvgP(rm.getRoomNo()));
+			}
+
 			return list;
 		}else {
 			throw new NotFoundException("검색 한 방이 없습니다");
@@ -90,6 +119,10 @@ public class RoomsServiceImpl implements RoomsService{
 		// TODO Auto-generated method stub
 		List<Room> list= roomDAO.searchByBreakfastStat(bfStat);
 		if(list!=null) {
+			for(Room rm : list) {
+				rm.setAvgScore(rs.getAvgP(rm.getRoomNo()));
+			}
+
 			return list;
 		}else {
 			throw new NotFoundException("검색 한 방이 없습니다");
@@ -100,6 +133,10 @@ public class RoomsServiceImpl implements RoomsService{
 	public List<Room> searchByResDate(String checkinDate, String checkoutDate) throws Exception {
 		List<Room> list= roomDAO.searchByResDate(checkinDate, checkoutDate);
 		if(list!=null) {
+			for(Room rm : list) {
+				rm.setAvgScore(rs.getAvgP(rm.getRoomNo()));
+			}
+
 			return list;
 		}else {
 			throw new NotFoundException("검색 한 방이 없습니다");
