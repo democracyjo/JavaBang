40a2a2175b23ac718ee882b5cd60abcbb78ad4e1
ckdud <ckdud@DESKTOP-M8JRTA8>
2021-03-17 10:41:11 +0900

    차영우 주석

--- src/kosta/mvc/model/dao/ReviewDAO.java
+++ src/kosta/mvc/model/dao/ReviewDAO.java
@@ -10,17 +10,26 @@ public interface ReviewDAO {
 	
 	/**
 	 * 방별 리뷰 보기
+	 * @param roomNo
+	 * @return List<Review>
+	 * @throws SQLException
 	 */
 	List<Review> selectReviewByRoomNo(int roomNo) throws SQLException;
 
 	
 	/**
 	 * 작성한 리뷰 보기
+	 * @param userNo
+	 * @return List<Review>
+	 * @throws SQLException
 	 */
 	List<Review> selectReviewByUserNo(int userNo) throws SQLException;
 	
 	/**
 	 * 리뷰 등록
+	 * @param review
+	 * @return
+	 * @throws SQLException
 	 */
 	int insertReview(Review review) throws SQLException;
 	
@@ -28,14 +37,21 @@ public interface ReviewDAO {
 	
 	/**
 	 * 리뷰 삭제
+	 * @param reviewNo
+	 * @return
+	 * @throws SQLException
 	 */
 	int deleteReview(int reviewNo) throws SQLException;
 
 
 	/**
 	 * 리뷰 수정하기
+	 * @param review
+	 * @return
+	 * @throws SQLException
 	 */
 	int updateReview(Review review) throws SQLException;
+	
 	/**
 	 * 리뷰 평점구하기
 	 * @param roomNo
--- src/kosta/mvc/model/service/ReviewService.java
+++ src/kosta/mvc/model/service/ReviewService.java
@@ -9,25 +9,37 @@ public interface ReviewService {
 
 	/**
 	 * πÊ∫∞ ∏Æ∫‰ ∫∏±‚
+	 * @param roomNo
+	 * @return List<Review>
+	 * @throws SQLException
 	 */
 	List<Review> selectReviewByRoomNo(int roomNo) throws SQLException;
 	/**
 	 * ¿€º∫«— ∏Æ∫‰ ∫∏±‚
+	 * @param userNo
+	 * @return List<Review>
+	 * @throws SQLException
 	 */
 	List<Review> selectReviewByUserNo(int userNo) throws SQLException;
 	
 	/**
 	 * ∏Æ∫‰ µÓ∑œ
+	 * @param review
+	 * @throws SQLException
 	 */
 	void insertReview(Review review) throws SQLException;
 	
 	/**
 	 * ∏Æ∫‰ ºˆ¡§
+	 * @param review
+	 * @throws SQLException
 	 */
 	void updateReview(Review review) throws SQLException;	
 	
 	/**
 	 * ∏Æ∫‰ ªË¡¶
+	 * @param reviewNo
+	 * @throws SQLException
 	 */
 	void deleteReview(int reviewNo) throws SQLException;
 	
